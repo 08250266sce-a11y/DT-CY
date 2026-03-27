ee431df4ebb526767958118f4c087640995cb8a5
unknown <08250266.sce@rub.edu.bt>
2026-03-27 09:40:19 -0600

    draft pair work

--- pairwork.py
+++ pairwork.py
@@ -10,19 +10,3 @@ print("Student Name:",name)
 print("Attendence:%.2f" %Attendence,"%")
 Average = (Total) / 4
 print("Averge:%.2f" %Average,"%")
-if (Average >= 50 and Attendence >=90):
-    print("Statuts: Pass")
-else:
-    print("Status: Fail")
-if (Average>=90 and Attendence>=90):
-    print("Grade:A[Excellent]")
-elif(Average>=80 and Attendence>=90):
-    print("Grade:B[v.Good]")
-elif(Average>=70 and Attendence>=90):
-    print("Grade:C[Good]")
-elif(Average>=60 and Attendence>=90):
-    print("GRade:D")
-if (Average>=90 and Attendence>=90):
-    print("You are Eligible for reward")
-else:
-    print("No Reward")
